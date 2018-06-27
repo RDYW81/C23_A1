64dab6396cfca01848d08ddb257faea6e413d43f
RDYW81 <r.daidone@t-online.de>
2018-06-27 17:10:07 +0200

    Formatierung, Bugfix

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 
 int main(void) {
 
-    printf("Minimal-Wert des Typs CHAR = %d\n", CHAR_MIN);
-    printf("Maximal-Wert des Typs CHAR = %d\n", CHAR_MAX);
+    printf("Minimal-Wert des Typs CHAR  = %d\n", CHAR_MIN);
+    printf("Maximal-Wert des Typs CHAR  =  %d\n", CHAR_MAX);
     printf("Minimal-Wert des Typs SHORT = %d\n", SHRT_MIN);
-    printf("Maximal-Wert des Typs SHORT = %d\n", SHRT_MAX);
-    printf("Minimal-Wert des Typs INT = %d\n", INT_MIN);
-    printf("Maximal-Wert des Typs INT = %d\n", INT_MAX);
-    printf("=====================================================\n");
+    printf("Maximal-Wert des Typs SHORT =  %d\n", SHRT_MAX);
+    printf("Minimal-Wert des Typs INT   = %d\n", INT_MIN);
+    printf("Maximal-Wert des Typs INT   =  %d\n", INT_MAX);
+    printf("=================================================\n");
     //printf("Minimal-Wert des Typs UNSIGNED CHAR = 0 bzw. existiert nicht\n");
-    printf("Maximal-Wert des Typs UNSIGNED CHAR = %d\n", UCHAR_MAX);
+    printf("Maximal-Wert des Typs UNSIGNED CHAR  = %d\n", UCHAR_MAX);
     //printf("Minimal-Wert des Typs UNSIGNED SHORT = 0 bzw. existiert nicht\n");
     printf("Maximal-Wert des Typs UNSIGNED SHORT = %d\n", USHRT_MAX);
     //printf("Minimal-Wert des Typs UNSIGNED INT = 0 bzw. existiert nicht\n");
-    printf("Maximal-Wert des Typs UNSIGNED INT = %d\n", UINT_MAX);
+    printf("Maximal-Wert des Typs UNSIGNED INT   = %u\n", UINT_MAX);
     printf("\n");
     printf("UNSIGNED CHAR, UNSIGNED SHORT und UNSIGNED INT existieren\n");
     printf("als solche nicht. Ihr Startwert ist jeweils 0.\n");
